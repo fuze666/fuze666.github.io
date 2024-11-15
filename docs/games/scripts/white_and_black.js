78ff572451aeb83936d0c080f7d2fe778797de14
fuze666 <tuaaribonnbe0313@gmail.com>
2024-11-15 17:08:40 +0900

    スリム化とブラシモードの変更とnpmのscriptsの変更

--- docs/games/scripts/white_and_black.js
+++ docs/games/scripts/white_and_black.js
@@ -1,21 +1,16 @@
 const game = function(p){
-
-  let winWidth = 400;
-  let winHeight = 300;
-  let isVisible = false;
-  let background_color = 0;
-  let flameCount = 0;
+  let weight = 10;
   let rectangles_colors = [];
 
   p.setup = () => {
-    p.createCanvas(winWidth,winHeight);
-    visibleButton = p.createButton("表示する");
-    visibleButton.id("visiblebutton");
-    visibleButton.mousePressed(() =>{
-      isVisible = !isVisible;
-      if(isVisible){
-        visibleButton.html("非表示");
-      }else visibleButton.html("表示");
+    p.createCanvas(400,300);
+    button = p.createButton("消す");
+    button.id("button");
+    button.mousePressed(() =>{
+      weight = -weight;
+      if(weight<0){
+        button.html("塗る");
+      }else button.html("消す");
     })
     
     for (let i = 0; i < 41; i++) {
@@ -27,24 +22,12 @@ const game = function(p){
   };
 
   p.draw = () => {
-    if(isVisible){
-      flameCount++;
       p.clear()
       draw_rectangles();
       paint_rects(p.mouseX*2-p.pmouseX,p.mouseY*2-p.pmouseY)
-    }else{
-      flameCount = 0;
-      p.background(p.random(5)+250);
-      p.noFill();
-      p.stroke(40);
-      p.strokeWeight(1)
-      p.rectMode(p.CORNER);
-      p.rect(0,0,p.width,p.height)
-    }
   };
 
   class rectangle_color{
-    color;
     constructor (original){
       this.color = original;
     }
@@ -52,16 +35,7 @@ const game = function(p){
       if(this.color>0&&this.color<256){
         this.color = Math.min(Math.max(this.color - weight,0),255);
       }
-      console.log("paint");
     }
-  }
-
-  function paint_rects(mousex,mousey){
-    paint_rect(mousex,mousey,20);
-    paint_rect(mousex-10,mousey,10);
-    paint_rect(mousex+10,mousey,10);
-    paint_rect(mousex,mousey-10,10);
-    paint_rect(mousex,mousey+10,10);
   }
 
   function draw_rectangles(){
@@ -77,12 +51,20 @@ const game = function(p){
       }
     }
   }
+
+  function paint_rects(mousex,mousey){
+    paint_rect(mousex,mousey,weight*2);
+    paint_rect(mousex-10,mousey,weight);
+    paint_rect(mousex+10,mousey,weight);
+    paint_rect(mousex,mousey-10,weight);
+    paint_rect(mousex,mousey+10,weight);
+  }
+
   function paint_rect(mousex,mousey,weight){
     x = Math.floor(Math.max(Math.min(mousex,400),0)/10);
     y = Math.floor(Math.max(Math.min(mousey,300),0)/10);
     rectangles_colors[x][y].paint(weight);
   }
-
 }
 
 new p5(game, 'game')
--- pug_generateIndex.js
+++ pug_generateIndex.js
@@ -6,7 +6,7 @@ function generateDataFile(inputFolder, outputFilePath) {
   // 指定されたフォルダ内のHTMLファイルを取得
   const files = fs.readdirSync(inputFolder).filter(file => file.endsWith('.html') && file !== 'index.html');
 
-  // 各ファイルのリンクと説明をJSON形式で生成
+  // 各ファイルのリンクをJSON形式で生成
   const data = {
     pages: files.map(file => ({
       name: path.parse(file).name,
@@ -18,4 +18,4 @@ function generateDataFile(inputFolder, outputFilePath) {
   fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2), 'utf-8');
   console.log(`Data file generated at: ${outputFilePath}`);
 }
-generateDataFile(path.join(__dirname, 'docs', 'games'), path.join(__dirname, 'templates', 'games', 'pagesData.json'));
+generateDataFile(path.join(__dirname, 'docs', process.argv[2]), path.join(__dirname, 'templates', process.argv[2], 'pagesData.json'));
